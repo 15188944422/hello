d4dde31e9040e68b7a1d48f35723503d66abf0d7
Song <2268992162@qq.com>
2022-09-22 20:13:24 +0800

    Linux_Hello Test

---
+++ Hello_Test/src/main/java/com/song/HelloTestApplication.java
@@ -0,0 +1,13 @@
+package com.song;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloTestApplication.class, args);
+    }
+
+}
